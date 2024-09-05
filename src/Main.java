e0f0b56433a3d7acc25b372aedf55d3b33d03415
Lauren Ferlito <lferlito@unc.edu>
2024-09-05 10:21:29 -0400

    Philosopher class instantiated and asymmetrical approach finished. Need to implement fairness still.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world! This is AJ.");
+    int numberOfPhilosophers = 5;
+    Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+    Object[] forks = new Object[numberOfPhilosophers];
+
+    // Create fork objects
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+      forks[i] = new Object();
+    }
+
+    // Create philosopher threads
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+      Object leftFork = forks[i];
+      Object rightFork = forks[(i + 1) % numberOfPhilosophers];
+
+      // Instantiate the philosopher objects and start the threads
+      philosophers[i] = new Philosopher(i, leftFork, rightFork);
+      philosophers[i].start();
+    }
   }
 }
-
---
+++ src/Philosopher.java
@@ -0,0 +1,46 @@
+public class Philosopher extends Thread {
+    private int id;
+    private Object leftFork;
+    private Object rightFork;
+
+    public Philosopher(int id, Object leftFork, Object rightFork) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    private void think() throws InterruptedException {
+        System.out.println("Philosopher " + id + " is thinking.");
+        Thread.sleep((int)(Math.random() * 100));
+    }
+
+    private void eat() throws InterruptedException {
+        System.out.println("Philosopher " + id + " is eating.");
+        Thread.sleep((int)(Math.random() * 100));
+    }
+
+    // Asymmetrical approach: even numbered philosophers take left first, odd numbered take right first
+    public void run() {
+        try {
+            while (true) {
+                think();
+
+                if (id % 2 == 0) {
+                    synchronized (leftFork) {
+                        synchronized (rightFork) {
+                            eat();
+                        }
+                    }
+                } else {
+                    synchronized (rightFork) {
+                        synchronized (leftFork) {
+                            eat();
+                        }
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
