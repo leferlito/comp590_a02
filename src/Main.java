8f05a6e4fe0d9af8904cf8875764b45dbb362cfa
Lauren Ferlito <lferlito@unc.edu>
2024-09-05 19:55:32 -0400

    Jk I added back the semaphore after doing some research, but honestly it doesn't seem to do THAT much to help. I added a printStatistics() function when the program terminates, so we can see some stats which has been helpful for me when comparing approaches.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
   public static int numberOfPhilosophers = 5;
+  public static Semaphore semaphore = new Semaphore(numberOfPhilosophers - 1);
 
   public static void main(String[] args) {
     Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
@@ -15,9 +18,16 @@ public class Main {
       Object leftFork = forks[i];
       Object rightFork = forks[(i + 1) % numberOfPhilosophers];
 
-      // Instantiate the philosopher objects and start the threads
-      philosophers[i] = new Philosopher(i, leftFork, rightFork);
+      philosophers[i] = new Philosopher(i, leftFork, rightFork, semaphore);
       philosophers[i].start();
     }
+
+    // Shutdown hook to collect and print data when program is terminated
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      System.out.println("\nProgram terminated. Printing statistics...");
+      for (Philosopher philosopher : philosophers) {
+        philosopher.printStatistics();
+      }
+    }));
   }
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,46 +1,84 @@
+import java.util.concurrent.Semaphore;
+
 public class Philosopher extends Thread {
     private int id;
     private Object leftFork;
     private Object rightFork;
+    private Semaphore semaphore;
 
-    public Philosopher(int id, Object leftFork, Object rightFork) {
+    // Tracking statistics to print at termination
+    private int eatCount = 0;
+    private long totalEatTime = 0;
+    private long totalWaitTime = 0;
+
+    public Philosopher(int id, Object leftFork, Object rightFork, Semaphore semaphore) {
         this.id = id;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        this.semaphore = semaphore;
     }
 
     private void think() throws InterruptedException {
         System.out.println("Philosopher " + id + " is thinking.");
-        Thread.sleep((int)(Math.random() * 100));
+        Thread.sleep((int)(Math.random() * 100));  // Random think time helps implement fairness
     }
 
     private void eat() throws InterruptedException {
+        long startEatTime = System.currentTimeMillis();  // Start logging eating time
+
         System.out.println("Philosopher " + id + " is eating.");
         Thread.sleep((int)(Math.random() * 100));
+
+        long endEatTime = System.currentTimeMillis();  // End logging eating time
+
+        // Update eat count and total eating time
+        eatCount++;
+        totalEatTime += (endEatTime - startEatTime);
     }
 
-    // Asymmetrical approach: even numbered philosophers take left first, odd numbered take right first
     public void run() {
         try {
             while (true) {
                 think();
 
+                // Measure wait time before eating
+                long waitStartTime = System.currentTimeMillis();
+
+                // Limit the number of philosophers trying to pick up forks to n-1 to aid fairness
+                semaphore.acquire();
+
                 if (id % 2 == 0) {
                     synchronized (leftFork) {
                         synchronized (rightFork) {
+                            // Wait time ends here before eating
+                            long waitEndTime = System.currentTimeMillis();
+                            totalWaitTime += (waitEndTime - waitStartTime);
                             eat();
                         }
                     }
                 } else {
                     synchronized (rightFork) {
                         synchronized (leftFork) {
+                            // Wait time ends here before eating
+                            long waitEndTime = System.currentTimeMillis();
+                            totalWaitTime += (waitEndTime - waitStartTime);
                             eat();
                         }
                     }
                 }
+
+                // Release the semaphore after eating, so other philosophers can pick up the forks
+                semaphore.release();
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    // Prints out stats once program is terminated
+    public void printStatistics() {
+        System.out.println("Philosopher " + id + " ate " + eatCount + " times.");
+        System.out.println("Philosopher " + id + " spent a total of " + totalEatTime + " ms eating.");
+        System.out.println("Philosopher " + id + " waited a total of " + totalWaitTime + " ms to eat.");
     }
 }
